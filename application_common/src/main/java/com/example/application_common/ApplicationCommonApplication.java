611d498465cd665ec1477a1ab6b607d71e22c699
xiaoyu2089 <zhangyu1290@163.com>
2021-04-15 15:38:39 +0800

    添加基础类库

---
+++ application_common/src/main/java/com/example/application_common/ApplicationCommonApplication.java
@@ -0,0 +1,14 @@
+package com.example.application_common;
+
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApplicationCommonApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ApplicationCommonApplication.class, args);
+    }
+
+}
