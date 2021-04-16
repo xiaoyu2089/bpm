b7c1b9a1bf5d34cb28e1bc3f1b4971a5531ef919
xiaoyu2089 <zhangyu1290@163.com>
2021-04-16 12:42:37 +0800

    引入Nacos Config 1. POM 中引用 spring-cloud-starter-alibaba-nacos-config 2. 必须使用bootstrab.yml，增加spring.cloud.nacos.config.server-addr、file-extension 3. 在nacos的配置文件中增加 【服务名】-【Profile名（dev、test、prod）】.yml的文件 4. 在使用配置文件的class头加上 @RefreshScope的注释

--- service_order/src/main/java/cn/com/taihe/service_order/controller/HomeController.java
+++ service_order/src/main/java/cn/com/taihe/service_order/controller/HomeController.java
@@ -2,6 +2,8 @@ package cn.com.taihe.service_order.controller;
 
 import cn.com.taihe.service_order.feign.SystemFeignClient;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -9,10 +11,14 @@ import javax.annotation.Resource;
 
 @RestController
 @RequestMapping("home")
+@RefreshScope
 public class HomeController {
 
     @Resource
     private SystemFeignClient systemFeignClient;
+
+    @Value("${abc}")
+    private String configValue;
 
     @RequestMapping("/index")
     public String index(){
@@ -21,6 +27,6 @@ public class HomeController {
         } catch (InterruptedException e) {
             e.printStackTrace();
         }
-        return "service_order home index|"+systemFeignClient.systemHomeIndex();
+        return configValue+"|service_order home index|"+systemFeignClient.systemHomeIndex();
     }
 }
